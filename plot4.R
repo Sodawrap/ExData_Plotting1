3713da2f5e835dcb56096a6c471f457d09f547a1
Amit Almagor <amit.almagor@mail.huji.ac.il>
2014-10-11 22:05:17 +0300

    Updated for direct png output

--- plot4.R
+++ plot4.R
@@ -12,6 +12,8 @@ rm(DSFull)
 datetime <- paste(as.Date(rdata$Date), rdata$Time)
 rdata$Datetime <- as.POSIXct(datetime)
 
+png("plot4.png",width=480,height=480) 
+
 ## Plot 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(rdata, {
@@ -23,12 +25,10 @@ with(rdata, {
        ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, bty="n",
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~Datetime, type="l", 
        ylab="Global Rective Power (kilowatts)",xlab="")
 })
 
-## save as png
-dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
