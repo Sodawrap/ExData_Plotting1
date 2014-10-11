f2388b18b707e6918e02627a4ae826f0052d22ab
Amit Almagor <amit.almagor@mail.huji.ac.il>
2014-10-11 21:39:37 +0300

    UPloading Project

---
+++ plot1.R
@@ -0,0 +1,21 @@
+
+## Read full DataSet (Might take a while)
+DSFull <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+DSFull$Date <- as.Date(DSFull$Date, format="%d/%m/%Y")
+
+## Subsetting required data and removing full DS
+rdata <- subset(DSFull, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(DSFull)
+
+## Converting dates to POSIX
+datetime <- paste(as.Date(rdata$Date), rdata$Time)
+rdata$Datetime <- as.POSIXct(datetime)
+
+## Create required histogram
+hist(rdata$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## save as png
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
